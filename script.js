de52331913f58a7a4e42e8dd9f6144df13ba9c65
Josh <tu_correo_de_YuriScorp.1M@gmail.com>
2025-08-24 17:13:25 -0500

    Login simple y estilos nuevos

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleTema() {
   const b = document.body;
   b.classList.toggle("dark");
 }
+
+function login(event) {
+  event.preventDefault(); // evita que la página se recargue
+
+  const usuario = document.getElementById("usuario").value;
+  const password = document.getElementById("password").value;
+
+  if (usuario === "admin" && password === "1234") {
+    alert("✅ Login correcto, bienvenido " + usuario);
+  } else {
+    alert("❌ Usuario o contraseña incorrectos");
+  }
+}
